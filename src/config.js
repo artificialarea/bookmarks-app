256977afe78f14f2d4ad2eeb454305e6c66a7651
artificialarea <sachased@gmail.com>
2020-07-24 14:15:48 -0700

    added EditBookmarks component for PATCH requests to db. All tests pass, but get failed prop type warnings, tho.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 export default {
-  API_ENDPOINT: `http://localhost:8000/bookmarks`,
-  API_KEY: process.env.REACT_APP_API_KEY,
+    API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+    API_KEY: process.env.REACT_APP_API_KEY,
+    // NOTE: in the .env.local 
+    // I had to add an 'escape' (\) before every instance of a dollar sign ($) in the API_KEY
+
 }
+
