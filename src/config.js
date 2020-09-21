affa8e3eade9aa82ee8c9c291e20c3b0e52b9971
Sacha Sedriks <sachased@gmail.com>
2020-09-21 14:21:18 -0700

    adjust localhost address

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 export default {
-    API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+    API_ENDPOINT: `http://localhost:8020/api/bookmarks`,
     API_KEY: process.env.REACT_APP_API_KEY,
     // NOTE: in the .env.local 
     // I had to add an 'escape' (\) before every instance of a dollar sign ($) in the API_KEY
 
+    // NOTE: changed name of file .env.local to just .env
 }
 
